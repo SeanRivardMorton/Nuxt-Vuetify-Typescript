20147aa1296190a6f54b856fcb67cd0a9bf235c1
Sean <sean.rivard.morton@gmail.com>
2018-05-22 15:21:51 -0400

    Basic storybook set-up

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure } from '@storybook/vue';
+
+import Vue from 'vue';
+// import Vuex from 'vuex'; // Vue plugins
+
+const req = require.context('../components', true, /\.story\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
